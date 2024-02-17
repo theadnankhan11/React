38b4acd089a66a3c44631888eecfa8bdb84e7ece
adnan-khan <akhan823974@gmail.com>
2024-02-17 16:08:35 +0530

    nike landing page

---
+++ project1/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Navigation from './components/Navigation'
+import HeroSection from './components/Hero'
+
+
+function App() {
+  return (
+    <div>
+    <Navigation/>
+    <HeroSection/>
+    </div>
+  )
+}
+
+export default App
